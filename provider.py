36300fe2b1570c7838a3ca411aa2680542c58b0e
chado13 <geaya13@gmail.com>
2024-05-18 22:30:50 +0900

    feat: add rs provider

--- provider.py
+++ provider.py
@@ -62,5 +62,38 @@ class SmaMomentumProvider(BaseDataProvider):
     async def fetch(self, factor: Factor, dt: datetime.datetime) -> pl.DataFrame:
         data = pl.read_csv("resource/ohlcv.csv").sort("dt")
         sma = get_ta_function("sma")(data["close"], self._window)
-        df = sma.with_columns()
+        momentum = (
+            sma.sort("dt")
+            .with_columns(pl.col("code").fill_null(0).pct_change().over("code").alias("return"))
+            .with_columns(pl.when(pl.col("return") > 0).then(0).otherwise(-1).alisa("momentum"))
+            .rolling(pl.col("momentum"), self._period)
+            .agg(pl.col("momentum").sum().alias("continued_momentum"))
+        )
         return momentum
+
+
+class RSProvider(BaseDataProvider):
+    def symbol_column_name(self) -> str:
+        return "code"
+
+    async def fetch(self, factor: Factor, dt: datetime.datetime) -> pl.DataFrame:
+        def _calc_rs(data: pl.DataFrame) -> pl.DataFrame:
+            df = data.join(index.select(["dt", "return"]), on="dt", suffix="_index").with_columns(
+                rs=pl.col("return") / pl.col("return_index")
+            )
+            return df.select(["dt", "code", "rs"])
+
+        index = (
+            pl.read_csv("resource/index.csv")
+            .sort("dt")
+            .with_columns(pl.col("close").pct_change().fill_null(0).over("code").alias("return"))
+            .select(["dt", "return"])
+        )
+        data = (
+            pl.read_csv("resource/ohlcv.csv")
+            .sort("dt")
+            .with_columns(pl.col("close").pct_change().fill_null(0).over("code").alias("return"))
+            .select(["dt", "code", "return"])
+        )
+        rs = data.group_by("code").apply(lambda x: _calc_rs(x)).select(["dt", "code", "rs"])
+        return rs
